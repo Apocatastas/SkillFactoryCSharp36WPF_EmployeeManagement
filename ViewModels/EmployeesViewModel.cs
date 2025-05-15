9d3c3aae50028630656b398dacd76c46a91f932e
apocatastas <molchanova.a.a@gmail.com>
2025-05-15 14:10:16 +0300

    unit 36.4

---
+++ ViewModels/EmployeesViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Repositories;
+using System.Collections.ObjectModel;
+
+namespace EmployeeManagement.ViewModels
+{
+    public class EmployeesViewModel
+    {
+        private EmployeeRepository _employeeRepository {  get; set; }
+        public EmployeesViewModel()
+        { 
+            _employeeRepository = new EmployeeRepository();
+        }
+
+        public ObservableCollection<Employee> Employees { get { return new ObservableCollection<Employee>(this._employeeRepository.GetAll()); }}
+
+    }
+}
